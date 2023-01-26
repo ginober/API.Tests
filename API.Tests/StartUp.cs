ab69995fd25cf2053fab699b4afd382fa4dc57d5
ginober <reginald.estember@yahoo.com>
2023-01-26 20:19:12 +0800

    [CODE_OPT] Added StartUp, Test Dependencies, Global Settings

---
+++ API.Tests/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+[Binding]
+public class StartUp 
+{
+    public static ServiceProvider ServiceProvider;
+    public StartUp()
+    {
+
+    }
+
+    [BeforeTestRun(Order = 0)]
+    public static void OneTimeSetup()
+    {
+        ServiceProvider = TestDependencies.CreateServices().BuildServiceProvider();
+    }
+}
---
+++ API.Tests/TestDependencies.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SolidToken.SpecFlow.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NUnit.Framework;
+using Microsoft.Extensions.Configuration;
+
+public static class TestDependencies
+{
+    [ScenarioDependencies]
+    public static IServiceCollection CreateServices()
+    {
+        var services = new ServiceCollection();
+        HostBuilderContext hostBuilderContext = null;
+        var hostbuilder = Host.CreateDefaultBuilder();
+        hostbuilder.
+                ConfigureAppConfiguration((ctx, cfg) =>
+                {
+                    hostBuilderContext = ctx;
+
+                    var env = TestContext.Parameters["environment"];
+                    //cfg.AddInMemoryCollection(TestContext.Parameters);
+                    
+                });
+
+        hostbuilder.Build();
+        ConfigureServices(services, hostBuilderContext);
+
+        return services;
+    }
+
+    private static void ConfigureServices(ServiceCollection services, HostBuilderContext hostBuilderContext)
+    {
+        services
+            .AddSingleton(hostBuilderContext.Configuration);
+    }
+}
